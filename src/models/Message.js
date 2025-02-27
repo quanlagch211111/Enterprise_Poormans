877afd96c03e9f7b5a827aa835b5649ecebe7210
Quan <quanlagch211111@fpt.edu.vn>
2025-02-27 14:37:36 +0700

    add chat function (1-1)

--- src/models/Message.js
+++ src/models/Message.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 
 const messageSchema = new mongoose.Schema({
     sender_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    receive_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    content: { type: String, required: true }, // Nội dung tin nhắn
+    receiver_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    content: { type: String, required: true }, 
     created_at: { type: Date, default: Date.now }
 });
 
-const Message = mongoose.model('Message', messageSchema);
+module.exports = mongoose.model("Message", messageSchema);
 
